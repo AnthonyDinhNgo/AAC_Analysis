e539c5add1d1b3505d14912607fef307e9a0dc75
AnthonyMyNgo <48073611+AnthonyMyNgo@users.noreply.github.com>
2019-07-28 01:20:05 -0700

    Added summary info scripts

---
+++ scripts/summary_info.R
@@ -0,0 +1,9 @@
+in_df <- read.csv("../data/aac_intakes.csv", stringsAsFactors = F)
+out_df <- read.csv("../data/aac_outcomes.csv", stringsAsFactors = F)
+in_out_df <- read.csv("../data/aac_intakes_outcomes.csv", stringsAsFactors = F)
+
+summary_info <- function(df){
+  return_list <- list(
+    
+  )
+}
